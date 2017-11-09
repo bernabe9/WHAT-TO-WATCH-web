3f77473cde64e17dde789ff03326916e4d63d54d
Bernabe Gonzalez <bernabe@rootstrap.com>
2017-11-08 23:50:28 -0300

    add button and styles

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -2,14 +2,12 @@ import React from 'react';
 
 const HomePage = () => (
   <div className="home-page">
-    <div>
-      <h1>WHAT TO WATCH?</h1>
-      <h4>
-        ¡La diversión está por comenzar! <br />
-        Estás a sólo un click de descubrir las películas perfectas para ti...
-      </h4>
-      <button>COMENZAR</button>
-    </div>
+    <h1>WHAT TO WATCH?</h1>
+    <h4>
+      ¡La diversión está por comenzar! <br />
+      Estás a sólo un click de descubrir las películas perfectas para ti...
+    </h4>
+    <button>COMENZAR</button>
   </div>
 );
 
